e1367358dff81b60ab71bc9165a8be0b5bd4d44e
Ondřej Winter <33571474+owinter92@users.noreply.github.com>
2021-03-11 21:05:32 +0100

    Update install_foam.sh

--- install_foam.sh
+++ install_foam.sh
@@ -2,5 +2,6 @@ sudo sh -c "wget --no-check-certificate -O - https://dl.openfoam.org/gpg.key | a
 sudo add-apt-repository http://dl.openfoam.org/ubuntu
 sudo apt-get update
 sudo apt-get -y install openfoam8
+export FOAM8_BASHRC=$FOAM8_BASHRC
 . $FOAM8_BASHRC
 mkdir -p $FOAM_RUN
